62c8360e9f5f12b96ebc2233d69d88fca22d48ba
bkjones <bkjones@andrew.cmu.edu>
2021-07-11 21:00:36 -0700

    maze generation algorithm basic functionality implemented

--- maze_search/maze_generator.py
+++ maze_search/maze_generator.py
@@ -10,6 +10,14 @@ agents to be deployed, and search algorithms to be tested.
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
 
 """
 MazeGenerator Class:
@@ -31,8 +39,10 @@ class MazeGenerator:
     def init_map(self):
 
         # init all values to infinity to mark them as unvisited
-        self.map = np.ones((map_size, map_size)) * np.inf
+        self.builder_map = np.ones((map_size, map_size)) * np.inf
+        self.map = np.ones((2 * map_size, 2 * map_size)) # Map is 2 times bigger to accommodate walls
 
+        self.builder_map = np.pad(self.builder_map, 1, constant_values=self.wall)
         self.map = np.pad(self.map, 1, constant_values=self.wall)
 
         self.starting_loc = (np.random.randint(1, map_size-1),
@@ -50,8 +60,13 @@ class MazeGenerator:
             current_cell = self.builder_stack.pop()
 
             # if the current cell has any neighbors which have not been visited:
-            if self.check_for_unvisited_neighbors(current_cell):
-                pass
+            new_cell, direction = self.check_for_unvisited_neighbors(current_cell)
+            if new_cell:
+                self.builder_stack.append(current_cell)
+                self.map[2 * new_cell[0], 2 * new_cell[1]] = self.free
+                self.map[2 * current_cell[0] + direction[0], 2 * current_cell[1] + direction[1]] = self.free
+                self.builder_map[new_cell[0], new_cell[1]] = self.free
+                self.builder_stack.append(new_cell)
 
     def check_for_unvisited_neighbors(self, cell):
         assert(isinstance(cell, tuple))
@@ -62,25 +77,31 @@ class MazeGenerator:
                                      [0, 1], # up
                                      [0, -1]]) # down
 
-
-        for i in range(neighbor_indices.shape[0]):
+        # permutation prevents biasing direction of expansion
+        for i in np.random.permutation(neighbor_indices.shape[0]):
             neighbor_x = cell[0] + neighbor_indices[i, 0]
             neighbor_y = cell[1] + neighbor_indices[i, 1]
-            if self.map[neighbor_x, neighbor_y] == np.inf:
-                return neighbor_x, neighbor_y
+            if self.builder_map[neighbor_x, neighbor_y] == np.inf:
+                return (neighbor_x, neighbor_y), (neighbor_indices[i, 0], neighbor_indices[i, 1])
 
-        return None
-
+        return None, None
 
     def visualize_map(self):
-        pass
+        fig = plt.figure()
+
+        ax = plt.subplot(111)
+
+        ax.imshow(self.map)
+
+        plt.show()
+
 
 
 if __name__ == "__main__":
-    map_size = int(500)
+    map_size = int(20)
 
     MazeGeneratorObj = MazeGenerator(map_size)
     MazeGeneratorObj.init_map()
     MazeGeneratorObj.create_map()
-
+    MazeGeneratorObj.visualize_map()
 
